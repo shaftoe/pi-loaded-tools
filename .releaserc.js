315b957d750b0e9fea7f5394879fd21eb74c3d64
Alexander Fortin <shaftoe@users.noreply.github.com>
2026-04-11 15:53:47 -0400

    ci: update semantic release to keep changelog tidy

--- .releaserc.js
+++ .releaserc.js
@@ -5,15 +5,35 @@
  * and package.json are updated, ensuring committed files always pass validation.
  */
 
+const changelogTitle = `# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]`;
+
 export default {
   branches: ["master"],
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        writerOpts: {
+          mainTemplate: `{{> header}}\n{{#each commitGroups}}{{#if title}}\n### {{title}}\n{{/if}}{{#each commits}}{{> commit root=@root}}{{/each}}{{/each}}{{> footer}}`,
+          headerPartial: `## [{{version}}] - {{date}}\n`,
+          commitPartial: `- {{#if scope}}**{{scope}}:** {{/if}}{{#if subject}}{{subject}}{{else}}{{header}}{{/if}}\n`,
+          footerPartial: `{{#if noteGroups}}{{#each noteGroups}}\n### {{title}}\n{{#each notes}}- {{#if commit.scope}}**{{commit.scope}}:** {{/if}}{{text}}\n{{/each}}{{/each}}{{/if}}`,
+        },
+      },
+    ],
     [
       "@semantic-release/changelog",
       {
         changelogFile: "CHANGELOG.md",
+        changelogTitle,
       },
     ],
     [
